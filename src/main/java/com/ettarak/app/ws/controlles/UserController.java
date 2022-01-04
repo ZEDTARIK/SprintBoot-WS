c5ba2983fbe1bcb091b0500b6577874faf847b55
zouhair <ettarak.zouhair@gmail.com>
2022-01-04 17:30:46 +0100

    Create Package -> controller

---
+++ src/main/java/com/ettarak/app/ws/controlles/UserController.java
@@ -0,0 +1,11 @@
+package com.ettarak.app.ws.controlles;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/users") // localhost:8080/users
+public class UserController {
+
+}
