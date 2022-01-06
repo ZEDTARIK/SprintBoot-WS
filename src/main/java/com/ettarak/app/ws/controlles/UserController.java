a26604a74a8feae2f4f224cab15f33380c6e888c
zouhair <ettarak.zouhair@gmail.com>
2022-01-06 09:32:15 +0100

    Generate REQUEST User class with properties and getters setters

--- src/main/java/com/ettarak/app/ws/controlles/UserController.java
+++ src/main/java/com/ettarak/app/ws/controlles/UserController.java
@@ -4,8 +4,11 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ettarak.app.ws.requests.UserRequest;
 
 
 @RestController
@@ -21,7 +24,7 @@ public class UserController {
 	}
 	
 	@PostMapping
-	public String createUser() {
+	public String createUser(@RequestBody UserRequest userRequest) {
 		return "create user was called!";
 	}
 
